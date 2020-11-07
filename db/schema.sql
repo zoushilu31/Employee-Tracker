1d0db9a5eb65d4af9c4d59dbab2feda248c2faf6
shilu <zoushilu@gmail.com>
2020-11-06 21:54:07 -0800

    added package.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,27 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE empolyee_db;
-USE employee_db;
+DROP DATABASE IF EXISTS employeeDB;
 
-CREATE TABLE departments (
-  id INTEGER(11) auto_increment NOT NULL,
-  name VARCHAR(20) NOT NULL,
-  PRIMARY KEY (id)
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,0) NOT NULL,
-  department_id INTEGER(11) NOT NULL,
-  PRIMARY KEY (id)
+CREATE TABLE role (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,2) NOT NULL,
+    department_id INT NOT NULL
 );
 
-CREATE TABLE employees(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  firstName VARCHAR(30),
-  lastName VARCHAR(30),
-  role_id INTEGER(11),
-  manager_id INTEGER(11) NULL,
-  PRIMARY KEY (id)
+
+
+CREATE TABLE employee (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL
 );
